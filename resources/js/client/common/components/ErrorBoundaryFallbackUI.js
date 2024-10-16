e7dd9f3754b3f08f903a135d15321b9458758c75
john-muinde <66753112+john-muinde@users.noreply.github.com>
2024-10-16 10:03:21 +0300

    added the build files

--- resources/js/client/common/components/ErrorBoundaryFallbackUI.js
+++ resources/js/client/common/components/ErrorBoundaryFallbackUI.js
@@ -36,7 +36,9 @@ const ErrorDetails = styled.pre`
 `;
 
 const isDevelopment =
-    !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+    typeof process !== "undefined" &&
+    process.env &&
+    (!process.env.NODE_ENV || process.env.NODE_ENV === "development");
 
 /**
  * Simple Error boundary fallback UI to display when error occurs
